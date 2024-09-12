9ae2a6da7a4e59010d43b4a3b210c4bad082a260
llindsa1 <lindsayuchiha@icloud.com>
2024-09-11 20:27:56 -0400

    ALAB 308.2.1

---
+++ Script.js
@@ -0,0 +1,19 @@
+// Part 1:
+const PI= 3.1415;
+const radius= 5;
+constant area= PI*radius*radius;
+
+const minimum space per plant= 0.8
+const number of plants=20;
+
+const capacity80=0.8*max capacity;
+const capcity50=0.5*max capacity;
+
+
+if (number of plants > capacity80) {
+    console.log (Capacity reached);
+} else if (number of plants>capacity50) {
+    console.log(Steady Growth);
+} else {
+    console.log(Grow more plants);
+}
